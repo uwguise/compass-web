251da6a4f5f827d34f84516b34f1c62768855fc5
MaazAli <maaz_ali@outlook.com>
2016-12-20 17:39:40 -0500

    Add bare equity-finder

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
+import EquityFinder from '../containers/equity-finder'
 
 function App() {
   return (
     <div>
-      Hello world!
+      <EquityFinder/>
     </div>
-  );
+  )
 }
 
-
-export default App;
+export default App
--- src/containers/equity-finder/index.js
+++ src/containers/equity-finder/index.js
@@ -2,22 +2,9 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 const EquityFinder = ({
-  company,
-  stockPrice,
-  dispatch,
 }) => {
-  console.log(company);
-  console.log(stockPrice);
-  console.log(dispatch);
   return (
     <h1>Sup Ladies</h1>
-    // <
-    //   subreddit={subreddit}
-    //   filter={filter}
-    //   threads={threads}
-    //   shouldShowImagePreview={shouldShowImagePreview(expandedImagePreviews)}
-    //   toggleImagePreview={toggleImagePreview(dispatch, subreddit)}
-    //   fetchThreadsOnNextPage={fetchThreadsOnNextPage(dispatch, subreddit, filter, finalThread)}/>
   );
 };
 
